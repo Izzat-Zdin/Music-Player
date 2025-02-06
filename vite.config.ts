20282bc9f54c574b3854d82e04f4eaac54c5a48a
Izzat-Zdin <izzatzdin375@gmail.com>
2025-02-06 15:51:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
       hot: false,
     }),
     ViteWebfontDownload([
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+      'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500&display=swap',
     ]),
     serviceWorker({
       manifest: {
